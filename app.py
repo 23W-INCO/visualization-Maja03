d56d9e9f9b6f4582968df5240290a9c6a3f0ab09
Maja Czekała <112630417+Maja03@users.noreply.github.com>
2024-01-25 17:52:12 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,55 @@
+import plotly.graph_objects as go
+import plotly.express as px
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+# Wizualizacja 1: Venn Diagram
+fig1 = go.Figure(go.Venn(
+    subsets=[set(depressed.index), set(anxious.index), set(panicking.index)],
+    set_labels=("Depressed", "Anxious", "Having Panic Attacks"),
+    set_colors=("orange", "purple", "green"),
+    opacity=0.9
+))
+fig1.update_layout(title="Conditions")
+
+# Wizualizacja 2: Bar Chart
+fig2 = go.Figure()
+
+for gender, gender_count in gender_counts.items():
+    fig2.add_trace(go.Bar(
+        x=labels,
+        y=gender_count,
+        name=gender,
+        marker_color=colors,
+        text=gender_count,
+        textposition='auto',
+    ))
+
+fig2.update_layout(title="Condition by Gender", xaxis=dict(tickangle=20), barmode='stack')
+
+# Osadzanie w aplikacji
+# Przykład aplikacji Dash
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children='Twoje Wizualizacje'),
+
+    html.Div(children=[
+        dcc.Graph(
+            id='venn-diagram',
+            figure=fig1
+        ),
+        dcc.Graph(
+            id='bar-chart',
+            figure=fig2
+        )
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
